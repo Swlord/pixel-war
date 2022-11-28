ba5891f3e975560391373bf2ccc353ad8b887912
Lindegaard <yann.lindegaard@student-cs.fr>
2022-11-28 08:50:52 +0100

    adding white

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const gridCellSize = 10
 const ctx = game.getContext('2d');
 const gridCtx = game.getContext('2d');
 
-const colorList = [
+const colorList = ["#FFFFFF",
     "#FFEBEE", "#FCE4EC", "#F3E5F5", "#B39DDB", "#9FA8DA", "#90CAF9", "#81D4FA", "#80DEEA",
     "#4DB6AC", "#66BB6A", "#9CCC65", "#CDD3C9", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722",
     "#A1887F", "#E0E0E0", "#90A4AE", "#000"]
