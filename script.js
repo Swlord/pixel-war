2901bd2773346871646a5f93e594a2f7e77e50c7
Lindegaard <yann.lindegaard@student-cs.fr>
2022-12-04 14:02:52 +0100

    adding colors

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const ctx = game.getContext('2d');
 const gridCtx = game.getContext('2d');
 
 const colorList = ["#FFFFFF",
-    "#FFEBEE", "#FCE4EC", "#F3E5F5", "#B39DDB", "#9FA8DA", "#90CAF9", "#81D4FA", "#80DEEA",
+    "#FFEBEE", "#FCE4EC", "#F3E5F5", "#B39DDB", "#9FA8DA", "#90CAF9", "#81D4FA", "#80DEEA", "#5E86B8", "#2F3476",
     "#4DB6AC", "#66BB6A", "#9CCC65", "#CDD3C9", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722",
-    "#A1887F", "#E0E0E0", "#90A4AE", "#000"]
+    "#D80000", "#A1887F", "#E0E0E0", "#90A4AE", "#000"]
 
 let CurrentColorChoice = colorList[9]
 
